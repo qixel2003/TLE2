14108ac330c646578d98d3cb22d21d45b9258e2b
1073500 <1073500@hr.nl>
2025-12-02 14:27:04 +0100

    Updated User model fillable attributes to include firstname, lastname, profile_picture, role, and classroom_id

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,13 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'firstname',
+        'lastname',
         'email',
         'password',
+        'profile_picture',
+        'role',
+        'classroom_id',
     ];
 
     /**
