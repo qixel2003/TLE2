71ca865d8864a8647a9643cb9505ad83dddcb8c3
Ishika <esoebhag@gmail.com>
2025-12-04 11:26:16 +0100

    details werkend (nog layout toevoegen en emoji's veranderen)

--- app/Models/route.php
+++ app/Models/route.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Arr;
 
-class route extends Model
+class Route extends Model
 {
 
     protected $fillable = [
