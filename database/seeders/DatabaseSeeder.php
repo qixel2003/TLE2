f94952d5103a67c7ba2c37539f9c04d3a2d80f5a
anemoia <killboy1100@gmail.com>
2025-12-08 11:55:22 +0100

    Removed unnecesary content from Branch
    Took 11 minutes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Classroom;
 use App\Models\Student;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -18,40 +17,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $student = User::factory()->create([
-            'name' => 'Mark',
-            'lastname' => 'Marcus',
-            'email' => 'mark@example.com',
-            'password' => Hash::make('password'),
-            'role' => 0,
-        ]);
 
-        $teacher = User::factory()->create([
-            'name' => 'Bobby',
-            'lastname' => 'Boberson',
-            'email' => 'bobby@example.com',
-            'password' => Hash::make('password'),
-            'role' => 1,
-        ]);
-
-        $admin = User::factory()->create([
-            'name' => 'Mary',
-            'lastname' => 'Marilyn',
-            'email' => 'mary@example.com',
-            'password' => Hash::make('password'),
-            'role' => 2,
-        ]);
-
-        $classroom = Classroom::create([
-            'user_id' => $teacher->id,
-            'name' => 'Class A',
-            'points' => 0,
-        ]);
-
-        Student::create([
-            'user_id' => $student->id,
-            'classroom_id' => $classroom->id,
-            'points' => 0,
-        ]);
     }
 }
