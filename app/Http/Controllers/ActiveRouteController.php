d6e3440ca1d4f234539eab91caee18c1dbe43989
anemoia <killboy1100@gmail.com>
2025-12-08 12:19:45 +0100

    Added dummy controllers
    Took 53 seconds

---
+++ app/Http/Controllers/ActiveRouteController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Active_Route;
+use Illuminate\Http\Request;
+
+class ActiveRouteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Active_Route $active_Route)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Active_Route $active_Route)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Active_Route $active_Route)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Active_Route $active_Route)
+    {
+        //
+    }
+}
