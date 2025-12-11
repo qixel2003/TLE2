df7660e6a135d1b852879ee112768cde4480a05c
anemoia <killboy1100@gmail.com>
2025-12-11 13:06:16 +0100

    Fixed minor syntax mistake again again
    Took 5 minutes

--- resources/views/role.blade.php
+++ resources/views/role.blade.php
@@ -3,11 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Natuurmonumenten - Bepaal je rol!</title>
-    <link rel="stylesheet" href={{asset('css/role.css')}}>
-    <link rel="stylesheet"
-          href={{asset('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css')}}>
-
+    <title>Natuurmonumenten - Kies je rol!</title>
+    <link rel="stylesheet" href={{asset('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css')}}>
     @vite('resources/css/role.css')
     @vite('resources/js/role.js')
 </head>
