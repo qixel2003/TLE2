c4bbdb1d833fea45a2c4501a16745e30e01a2144
RexTheWeeb <D.F.vantwoudt@outlook.com>
2025-12-17 11:20:38 +0100

    More layout additions.

--- resources/views/photos/edit.blade.php
+++ resources/views/photos/edit.blade.php
@@ -1,28 +1,30 @@
-<x-layout>
+@extends('layouts.natuurMonumenten')
+
+@section('content')
     <x-slot:heading>
         <h1>Foto bewerken</h1>
     </x-slot:heading>
 
 
-        <form action="{{ route('photos.update', $photo) }}" method="POST" enctype="multipart/form-data">
-            @csrf
+    <form action="{{ route('photos.update', $photo) }}" method="POST" enctype="multipart/form-data">
+        @csrf
 
-            <img src="{{ asset('storage/' . $photo->image_path) }}">
+        <img src="{{ asset('storage/' . $photo->image_path) }}">
 
-            <label>Titel</label>
-            <input type="text" name="title" value="{{ $photo->title }}">
+        <label>Titel</label>
+        <input type="text" name="title" value="{{ $photo->title }}">
 
-            <label>Beschrijving</label>
-            <textarea name="description">{{ $photo->description }}</textarea>
+        <label>Beschrijving</label>
+        <textarea name="description">{{ $photo->description }}</textarea>
 
-            <label>Nieuwe foto (optioneel)</label>
-            <input type="file" name="photo">
+        <label>Nieuwe foto (optioneel)</label>
+        <input type="file" name="photo">
 
-            <button type="submit">Opslaan</button>
-        </form>
+        <button type="submit">Opslaan</button>
+    </form>
 
-        <form action="{{ route('photos.destroy', $photo) }}" method="POST" class="mt-4">
-            @csrf
-            <button type="submit" onclick="return confirm('Verwijderen?')">Verwijder foto</button>
-        </form>
-</x-layout>
+    <form action="{{ route('photos.destroy', $photo) }}" method="POST" class="mt-4">
+        @csrf
+        <button type="submit" onclick="return confirm('Verwijderen?')">Verwijder foto</button>
+    </form>
+@endsection
